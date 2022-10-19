4f896a7d94be7f9f7f8969d53ba02fe607e9f50e
allanzaparoli <allanzaparoli@gmail.com>
2022-10-19 17:12:54 -0300

    Requisito 2

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,19 @@
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  static counter = 0;
+  private _maxLifePoints: number;
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+    Dwarf.counter += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Dwarf.counter;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
---
+++ src/Races/Orc.ts
@@ -0,0 +1,19 @@
+import Race from './Race';
+
+export default class Orc extends Race {
+  static counter = 0;
+  private _maxLifePoints: number;
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 74;
+    Orc.counter += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc.counter;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
